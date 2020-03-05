9d500438f8e1d04fb98ea810683685e5ebf484d7
James Ginns <starvagrant@yahoo.com>
2020-03-05 11:06:29 -0600

    Have Job class inherit from abstract entity

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
@@ -3,41 +3,16 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 import javax.persistence.*;
 
 @Entity
-public class Job{
+public class Job extends AbstractEntity {
 
-    @Id
-    @GeneratedValue
-    private int id;
-
-    private String name;
-
-    private String employer;
     private String skills;
 
     public Job() {
     }
 
-    public Job(String anEmployer, String someSkills) {
-        super();
-        this.employer = anEmployer;
-        this.skills = someSkills;
-    }
-
-    // Getters and setters.
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmployer() {
         return employer;
     }
 
-    public void setEmployer(String employer) {
         this.employer = employer;
     }
 
