e92f9419920710cd6ff3a39b26634dfffc07c3fa
James Ginns <starvagrant@yahoo.com>
2020-03-04 18:48:41 -0600

    Finish Part #1
    Update the queries.sql with the requisite information as define
    in the assignment instructions.

--- queries.sql
+++ queries.sql
@@ -1,4 +1,8 @@
 ## Part 1: Test it with SQL
+  `id` int NOT NULL, # PRIMARY KEY
+  `employer` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `skills` varchar(255) DEFAULT NULL,
 
 ## Part 2: Test it with SQL
 
