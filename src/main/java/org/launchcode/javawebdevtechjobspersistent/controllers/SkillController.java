d76b4ff37da8a7a385203e9634f431695cc6f5c9
James Ginns <starvagrant@yahoo.com>
2020-03-04 19:59:49 -0600

    Add view / process logic to skill controller
    Use a skillRepository to add persistent value to the database
    and retrieve it for a view. Note that the skills template displays
    an error because it requires a Many to Many relationship which
    hasn't been defined yet.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/SkillController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/SkillController.java
@@ -1,4 +1,57 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
+import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("skills")
 public class SkillController {
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @GetMapping("")
+    public String displaySkillList(Model model) {
+        model.addAttribute("title", "All skills");
+        model.addAttribute("skills", skillRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("add")
+    public String renderSkillForm(Model model) {
+        model.addAttribute(new Skill());
+        return "skills/add";
+    }
+
+    @PostMapping("add")
+    public String ProcessAddSkillForm(@ModelAttribute @Valid Skill skill, Errors errors, Model model){
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add a skill");
+            return "redirect:";
+        }
+        skillRepository.save(skill);
+        return "redirect:../";
+    }
+
+    @GetMapping("view/{skillId}")
+    public String displayViewSkills(Model model, @PathVariable int skillId) {
+        Optional optSkill = skillRepository.findById(skillId);
+        if (optSkill.isPresent()) {
+            Skill skill = (Skill) optSkill.get();
+            model.addAttribute("skill", skill);
+            return "skills/view";
+        } else {
+            return "redirect:../";
+        }
+    }
 }
