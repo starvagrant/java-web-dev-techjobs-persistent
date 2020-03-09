8f36c989b49bc526928f44d3d89d87718f659223
James Ginns <starvagrant@yahoo.com>
2020-03-08 19:55:29 -0500

    Make a Home controller to persist job entries
    Make a Home controller that persists job entries, with one
    required employer and many optional skills.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -1,19 +1,37 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
 import org.launchcode.javawebdevtechjobspersistent.models.Job;
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.JobRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
  */
 @Controller
 public class HomeController {
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
 
     @RequestMapping("")
     public String index(Model model) {
@@ -27,6 +45,9 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
+        model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
+
         return "add";
     }
 
@@ -34,11 +55,30 @@ public class HomeController {
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
                                        Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
 
+
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
+            model.addAttribute(new Job());
+            model.addAttribute("employers", employerRepository.findAll());
+            model.addAttribute("skills", skillRepository.findAll());
+          //  model.addAttribute("formErrors", errors.getFieldErrorCount());
+            List<ObjectError> objectErrors = errors.getAllErrors();
+            model.addAttribute("objectErrors", objectErrors);
             return "add";
         }
 
+        Optional<Employer> jobEmployer = employerRepository.findById(employerId);
+        Iterable<Skill> iterableSkills = skillRepository.findAllById(skills);
+        List<Skill> jobSkills = new ArrayList<>();
+        for (Skill sk: iterableSkills) {
+            newJob.getSkills().add(sk);
+        }
+
+        // A job must have an employer
+        if (jobEmployer.isPresent()) {
+            newJob.setEmployer(jobEmployer.get());
+            jobRepository.save(newJob);
+        }
         return "redirect:";
     }
 
